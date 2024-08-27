1e99d6af1d3d527d1ff3f80e44c1f230ae20a5df
pacolu13 <lucavelazquez13@gmail.com>
2024-08-27 20:49:20 -0300

    Update caracteristicas

--- js/cargas/cargarProductos.js
+++ js/cargas/cargarProductos.js
@@ -21,13 +21,13 @@ function createProduct(producto) {
     let template = `
     <li class="list-group-item d-flex justify-content-between align-items-start">
     <div class="ms-2 me-auto">
-      <div class="fw-bold">${producto.nombre} - ${producto.tipo} </div>
-      Stock: ${producto.stockdisponible}
-      <div>Stock Minimo: ${producto.stockminimo}</div>
+      <div class="fw-bold">${producto.Nombre} - ${producto.Tipo} </div>
+      Stock: ${producto.StockDisponible}
+      <div>Stock Minimo: ${producto.StockMinimo}</div>
     </div>
     <div class="button-trash">
-        <a href="#" onclick="eliminarProducto(${producto.ID})"><i class="fa-solid fa-trash"></i></a>
-        <a href="#" onclick="actualizarProducto(${producto.ID})"><i class="fa-solid fa-gear"></i></a>
+        <a href="#" onclick="eliminarProducto(${producto.CodigoUnico})"><i class="fa-solid fa-trash"></i></a>
+        <a href="#" onclick="actualizarProducto(${producto.CodigoUnico})"><i class="fa-solid fa-gear"></i></a>
     </div>
     </li>`;
     return template;
