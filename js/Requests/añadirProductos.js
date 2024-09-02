5a75b9374772a8ef9d2dc497e3490aaaa7d6b3bd
pacolu13 <lucavelazquez13@gmail.com>
2024-09-01 23:00:35 -0300

    Update añadirProductos.js

--- js/Requests/añadirProductos.js
+++ js/Requests/añadirProductos.js
@@ -1,21 +1,11 @@
 function añadirProducto(Productos) {
 
-    try {
-        //DECLARAR URL DE PRODUCTOS
-        const response = fetch(urlProductos, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(Productos) // Convertir el producto a una cadena JSON
-        });
-
-        if (!response.ok) {
-            generarError(response.statusText);
-        }
-        //PESTAÑA EMERGENTE DE QUE SE AÑADIERON CORRECTAMENTE
-        
-    } catch (error) {
-        generarError(error);
-    }
+    //DECLARAR URL DE PRODUCTOS
+    fetch(urlProductos, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(Productos) // Convertir el producto a una cadena JSON
+    });
 }
